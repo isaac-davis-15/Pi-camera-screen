7314e914d2816374b08e1e437b8a19c0d2a7392f
Isaac Davis <gamer.kid.71321@gmail.com>
2017-02-25 09:44:15 -0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -2,6 +2,7 @@ from import picamera, Color
 import os.path as path
 import Image 
 import time
+import psutil
 
 camera = picamera.PiCamera()
 
@@ -13,14 +14,19 @@ def img_namer():
     return
     
 def take_pic():
+  #start the preview
   camera.start_preview();
+  #start countdown
   count_down = 5
+  camera.annotate_background = Color("White")
   for x in range(0, 5):
     camera.annotate_text = count_down
     count_down--
     time.sleep(1)
-  camera.annotate_background = "white"
   time.sleep(.135)
+  #create a shudder effect
+  shutter_img = Image.open("shutter_img")
+  shutter_img.show();
   camera.capture('image_' + count + ".jpg")
   count++
   time.sleep(.135)
